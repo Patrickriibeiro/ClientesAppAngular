32d922fe2b5789e7dd7e7707873bdec499f84e7e
Patrick <patrick.oliveira@sulamerica.com.br>
2021-11-29 22:44:12 -0300

    ajustanto pack repository

--- ClienteApp/src/main/java/com/pro/ClienteApp/model/repository/ClienteRepository.java
+++ ClienteApp/src/main/java/com/pro/ClienteApp/model/repository/ClienteRepository.java
@@ -1,4 +1,4 @@
-package com.pro.ClienteApp.repository;
+package com.pro.ClienteApp.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
